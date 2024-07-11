3cb62615bf03a40e8182aec699464594488050da
Amit Atav <amit.benatav@gmail.com>
2024-07-11 14:41:42 +0300

    forgot password and update email

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,28 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
-const mongoose = require('mongoose');
+
+router.patch('/users/forgot-password', async (req, res) => {
+  try {
+    const { email, newPassword, authPassword } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send('לא נמצא אימייל במערכת');
+    }
+    if (newPassword !== authPassword) {
+      return res.status(400).send('אימות סיסמה נכשל');
+    }
+
+    user.password = newPassword;
+    await user.save();
+    const token = await user.generateToken();
+    res.send({ user, token });
+
+    res.send();
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -41,15 +62,31 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 });
 
+router.get('/users/me', auth, async (req, res) => {
+  try {
+    const email = req.user.email;
+    res.send({email});
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowed = ['name', 'password'];
+  const allowed = ['email'];
   const validUpdate = updates.every((update) => allowed.includes(update));
 
   if (!validUpdate) {
     return res.status(400).send({ error: 'invald updates!!!' });
   }
+  if (req.user.email === req.body.email) {
+    return res.status(400).send('האימייל החדש לא יכול להיות האימייל הישן');
+  }
   try {
+    const anotherUser = await User.findOne({ email: req.body.email });
+    if (anotherUser) {
+      return res.status(400).send('אימייל קיים במערכת');
+    }
     updates.forEach((update) => (req.user[update] = req.body[update]));
     await req.user.save();
     res.send(req.user);
